3da7050a35818915aa49294b7feafd3148994c8e
Scott <wsc@iri-labs.com>
2019-01-25 15:35:27 +0100

    licensing

--- doc.go
+++ doc.go
@@ -1,2 +1,5 @@
+// Copyright 2018 Scott Cotton. All rights reserved.  Use of this source
+// code is governed by a license that can be found in the License file.
+
 // Package hilbert performs an approximate hilbert transform.
 package hilbert
